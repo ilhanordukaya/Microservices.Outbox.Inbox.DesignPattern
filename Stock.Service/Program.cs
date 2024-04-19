d93391c09a99a53e403cb354c92a1677ee3ea0d3
ilhanordukaya <ilhanordukaya69@gmail.com>
2024-04-20 02:44:54 +0300

    outbox inbox pattern configuration

--- Stock.Service/Program.cs
+++ Stock.Service/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Shared;
+using Stock.Service.Consumers;
+using Stock.Service.Models.Contexts;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddDbContext<StockDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MSSQLServer")));
+
+builder.Services.AddMassTransit(configurator =>
+{
+	configurator.AddConsumer<OrderCreatedEventConsumer>();
+	configurator.UsingRabbitMq((context, _configure) =>
+	{
+		_configure.Host(builder.Configuration["RabbitMQ"]);
+
+		_configure.ReceiveEndpoint(RabbitMQSettings.Stock_OrderCreatedEvent, e => e.ConfigureConsumer<OrderCreatedEventConsumer>(context));
+	});
+});
+var host = builder.Build();
+host.Run();
